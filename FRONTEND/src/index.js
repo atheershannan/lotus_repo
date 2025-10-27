2a8bf500b2db802f9bc51c51dc32f764c04cfc06
atheershannan <atheershannan1011@gmail.com>
2025-10-27 02:46:01 +0200

    fix: Complete frontend setup - remove LocalizationProvider

--- FRONTEND/src/index.js
+++ FRONTEND/src/index.js
@@ -4,8 +4,6 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 
 import App from './App';
 import { store } from './store/store';
@@ -20,9 +18,7 @@ root.render(
       <BrowserRouter>
         <ThemeProvider theme={theme}>
           <CssBaseline />
-          <LocalizationProvider dateAdapter={AdapterDateFns}>
-            <App />
-          </LocalizationProvider>
+          <App />
         </ThemeProvider>
       </BrowserRouter>
     </Provider>
